8a6b24810b7822f7d8170d7660a6d7ad0d0557fb
Lorraine Rezende <lorrainesrezende@gmail.com>
2022-01-13 10:15:14 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,26 @@
+import { ReactComponent as GithubIcon } from "assets/img/GithubIcon.svg";
+import "./styles.css";
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a
+            href="https://github.com/lorrainesrezende"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/lorrainesrezende</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
